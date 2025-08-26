d38610b0e88198dc327191558cd330012ec07ba5
ksd1346 <56496880+ksd1346@users.noreply.github.com>
2025-08-27 00:54:42 +0900

    Counting DNA Nucleotides

--- bioinformatics stronghold/Counting DNA Nucleotides.pl
+++ bioinformatics stronghold/Counting DNA Nucleotides.pl
@@ -1,5 +1,10 @@
-use v6;
+use strict;
+use warnings;
 
-my $s = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC";
+my $seq = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC";
+my $count_A = $seq =~ tr/A//;
+my $count_T = $seq =~ tr/T//;
+my $count_G = $seq =~ tr/G//;
+my $count_C = $seq =~ tr/C//;
 
-$s.comb.Bag.Hash.say
+print "$count_A $count_T $count_G $count_C";
